441bae5310b185a6ccc67d36bd40467e871c2314
mirzoo <mirzo.okhunov@gmail.com>
2022-10-21 12:12:58 +0300

    add pages news

--- js/script.js
+++ js/script.js
@@ -7,47 +7,18 @@ $(document).ready(function() {
 
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
 
-    async function formSend(e) {
-        e.preventDefault();
+const anchors = document.querySelectorAll('a[href*="#"]')
 
-        let error = formValidate(form);
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
 
-        if (error === 0) {
-            window.location.href = './check.html'
-        }
+    const blockID = anchor.getAttribute('href').substr(1)
 
-
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
 }
-
-    function formValidate(form) {
-        let error = 0;
-        let formReq = document.querySelectorAll('._req');
-
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            formRemoveError(input);
-
-            if(input.value === '') {
-                formAddError(input);
-                error++;
-            }
-
-            return error;
-        }
-    }
-    function formAddError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-
-    function formRemoveError(input) {
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-    }
-
-});
-
